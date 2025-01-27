4456900b431166c4c8466b93989a1c793b389c4c
jairom94 <jairom94@gmail.com>
2025-01-26 19:36:48 -0500

    Muestra preview poster

--- app/controllers.py
+++ app/controllers.py
@@ -1,34 +1,38 @@
 import tkinter as tk
 
 from app.views import Consultor,Action
-from app.models import Consultor as Buscador
+from app.models import Consultor as Buscador,Film
+
+
 
 class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
         self.title("BLOCKBUSTER")
-        self.frm_consultor = Consultor(self,self.fun_buscar,self.fun_limpiar,\
+        frm_consultor = Consultor(self,self.fun_buscar,self.fun_limpiar,\
                                     self.fun_favoritos,self.fun_guardar,\
-                                    self.fun_borrar)
-        self.frm_consultor.pack()
+                                    self.fun_borrar,self.show_pelicula)
+        frm_consultor.pack()
+        self.panel_peliculas = frm_consultor.panel_peliculas
+        self.panel_detalle_pelicula = frm_consultor.panel_detalle_pelicula 
         self.busca = Buscador()
 
 
     def fun_buscar(self,action:Action):
         # print(action)
-        titulo = self.frm_consultor.panel_peliculas.txt_buscar.text.get()
+        titulo = self.panel_peliculas.txt_buscar.text.get()
         self.busca.titulo = titulo
         self.busca.consultar()
         listado = [(peli.id,peli.titulo,peli.anio) for peli in self.busca.peliculas]
-        self.frm_consultor.panel_peliculas.list_peliculas.items = listado
-        self.frm_consultor.panel_peliculas.list_peliculas.show()
+        self.panel_peliculas.list_peliculas.items = listado
+        self.panel_peliculas.list_peliculas.show()
         
         
 
 
     def fun_limpiar(self,action:Action):
-        self.frm_consultor.panel_peliculas.list_peliculas.clear()
+        self.panel_peliculas.list_peliculas.clear()
         self.busca = Buscador()
 
     def fun_favoritos(self,action:Action):
@@ -39,6 +43,15 @@ class App(tk.Tk):
     
     def fun_borrar(self,action:Action):
         print(action)
+
+    def show_pelicula(self,titulo:str):        
+        film:Film = self.busca.pelicula_seleccionada(titulo)
+        self.panel_detalle_pelicula.txt_titulo.text.set(film.titulo)
+        self.panel_detalle_pelicula.txt_director.text.set(film.director)
+        self.panel_detalle_pelicula.txt_anio.text.set(film.anio)
+        self.panel_detalle_pelicula.img_poster.url = film.poster        
+        self.panel_detalle_pelicula.img_poster.preview_poster()
+
 
     def run(self):
         self.mainloop()
--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ from PIL import Image as Img
 from PIL import ImageTk
 from enum import Enum,auto
 from typing import Callable,Dict,Any
+import requests
+import os
+
+from http import HTTPStatus
 
 class Action(Enum):
     BUSCAR = auto()
@@ -66,12 +70,10 @@ class ListView(tk.Frame):
         select_item = self.list.focus()        
         if select_item:
             item_data=self.list.item(select_item)
-            print(item_data)
             titulo,_=item_data.get('values',None)
-            self.__item_selected(titulo)
-            
-            item_id = item_data['text']
-            print(item_id)
+            self.__item_selected(titulo)            
+            # item_id = item_data['text']
+            # print(item_id)
 
     def add(self,item:list):
         self.items.append(item)
@@ -94,89 +96,68 @@ class Label(tk.Frame):
         lbl.pack(side=tk.TOP,expand=True,fill=tk.X)
 
 class Image(tk.Frame):
-    def __init__(self,parent,path='',**kwargs):
+    def __init__(self,parent,url='',name_image='',**kwargs):
         super().__init__(parent,width=kwargs.get('width'),\
         height=kwargs.get('height'),highlightbackground='black',highlightthickness=1)
         self.pack_propagate(False)
         self.lbl_marco = tk.Label(self)
         self.lbl_marco.pack(side=tk.TOP,expand=True,fill=tk.BOTH)
-        if path:
-            with open(f"{path}", "rb") as f:
+        self.path = None
+        self.url = url
+        self.name_image = name_image
+        self.imagen_cache = None       
+    
+    def show_image(self):
+        if self.path:
+            with open(f"{self.path}", "rb") as f:
                 image_bytes = f.read()
                 # print(image_bytes)
             image_data = Img.open(BytesIO(image_bytes))
             output = BytesIO()
             image_data.save(output,format="PNG")
             output.seek(0)
-            self.img = ImageTk.PhotoImage(file=output)
-            self.lbl_marco.config(image=self.img)
+            self.imagen_cache = ImageTk.PhotoImage(file=output)
+            self.lbl_marco.config(image=self.imagen_cache)
+    
+    def download_image(self):
+        if self.url:
+            response_img = requests.get(self.url)
+            os.makedirs("poster", exist_ok=True)
+            with open(f'poster/{self.name_image}.jpg','wb') as img:
+                img.write(response_img.content)
+    
+    def preview_poster(self):
+        response_img = requests.get(self.url)        
+        if response_img.status_code == HTTPStatus.OK:            
+            img_data = BytesIO(response_img.content)
+            img = Img.open(img_data)
+
+            converted_img = BytesIO()
+            img.convert("RGBA").save(converted_img,format="PNG")
+            converted_img.seek(0)
+
+            tk_img = ImageTk.PhotoImage(Img.open(converted_img))
+            # print(tk_img)
+            self.imagen_cache = tk_img
+            self.lbl_marco.config(image=self.imagen_cache)
+
+
+
+
 
 
 class Consultor(tk.Frame):
     def __init__(self,parent,fun_buscar,fun_limpiar,fun_favoritos,\
-                fun_guardar,fun_borrar,**kwargs):
+                fun_guardar,fun_borrar,show_pelicula,**kwargs):
         super().__init__(parent)        
-        self.panel_peliculas = PanelPeliculas(self,fun_buscar,fun_limpiar,fun_favoritos)
+        self.panel_peliculas = PanelPeliculas(self,fun_buscar,fun_limpiar,\
+                                            fun_favoritos,show_pelicula)
         self.panel_peliculas.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,pady=10,padx=10)
         
         self.panel_detalle_pelicula = PanelPeliculaDetalle(self,\
                                         fun_guardar,fun_borrar)
         self.panel_detalle_pelicula.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,pady=10,padx=10)
-        #(side=tk.LEFT,fill=tk.BOTH,expand=True,pady=10,padx=10)
-        # frm_header = tk.Frame(frmIzq,width=300)
-        # frm_header.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-        # self.txt_buscar = TextField(parent=frm_header,height=30)
-        # self.txt_buscar.pack(side=tk.LEFT,expand=True,fill=tk.X)
-
-        # btn_buscar = BtnCustom(parent=frm_header,text='Buscar',width=60,\
-        #                             height=30,\
-        #                             fun_args=kwargs.get('fun_args'),\
-        #                             fun_delegada=kwargs.get('fun_delegada'))
-        # btn_buscar.pack(side=tk.LEFT,padx=5)
-
-        # label = tk.Label(frmIzq,text='RESULTADOS',anchor=tk.W)
-        # label.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
-
-        # listview = ListView(parent=frmIzq,width=300,height=400,headers=['Titulo','Año'])
-        # listview.pack(side=tk.TOP,expand=True,fill=tk.BOTH)
-
-        # btn_limpiar = BtnCustom(parent=frmIzq,text='Limpiar',height=30,)
-        # btn_limpiar.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
-
-        # btn_favoritos = BtnCustom(parent=frmIzq,text='Cargar Favoritos',height=30)
-        # btn_favoritos.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
-
-        # frmDer = tk.Frame(self)
-        # frmDer.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,pady=10,padx=10)
-
-
-        # lbl_titulo = tk.Label(frmDer,anchor=tk.W,text='Titulo')
-        # lbl_titulo.pack(side=tk.TOP,expand=True,fill=tk.X)
-        # txt_titulo = TextField(parent=frmDer,height=30,width=300)
-        # txt_titulo.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
-
-        # lbl_director = tk.Label(frmDer,anchor=tk.W,text='Director')
-        # lbl_director.pack(side=tk.TOP,expand=True,fill=tk.X)
-        # txt_director = TextField(parent=frmDer,height=30,width=300)
-        # txt_director.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
-
-        # lbl_anio = tk.Label(frmDer,anchor=tk.W,text='Año')
-        # lbl_anio.pack(side=tk.TOP,expand=True,fill=tk.X)
-        # txt_anio = TextField(parent=frmDer,height=30,width=300)
-        # txt_anio.pack(side=tk.TOP,expand=True,fill=tk.X, pady=5)
-
-        # img_poster = Image(parent=frmDer,width=300,height=300,path='')
-        # img_poster.pack(side=tk.TOP,expand=True,fill=tk.BOTH,pady=10)
-
-        # frm_buttons = tk.Frame(frmDer,relief=tk.GROOVE)
-        # frm_buttons.pack(side=tk.TOP,expand=True,fill=tk.X)
-
-        # btn_guardar = BtnCustom(frm_buttons,'Guradar',Action.GUARDAR,funcion,height=30,width=70,)
-        # btn_guardar.pack(side=tk.RIGHT,fill=tk.X)
-
-        # btn_borrar = BtnCustom(frm_buttons,'Borrar',Action.BORRAR,funcion,height=30,width=60)
-        # btn_borrar.pack(side=tk.RIGHT,fill=tk.X,padx=10)
+        
 
 class PanelPeliculas(tk.Frame):
     def __init__(self,parent,fun_buscar,fun_limpiar,fun_favoritos,show_pelicula,**kwargs):
@@ -212,21 +193,21 @@ class PanelPeliculaDetalle(tk.Frame):
         # self.pack_propagate(False)
         lbl_titulo = tk.Label(self,anchor=tk.W,text='Titulo')
         lbl_titulo.pack(side=tk.TOP,expand=True,fill=tk.X)
-        txt_titulo = TextField(self,height=30,width=300)
-        txt_titulo.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
+        self.txt_titulo = TextField(self,height=30,width=300)
+        self.txt_titulo.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
 
         lbl_director = tk.Label(self,anchor=tk.W,text='Director')
         lbl_director.pack(side=tk.TOP,expand=True,fill=tk.X)
-        txt_director = TextField(self,height=30,width=300)
-        txt_director.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
+        self.txt_director = TextField(self,height=30,width=300)
+        self.txt_director.pack(side=tk.TOP,expand=True,fill=tk.X,pady=5)
 
         lbl_anio = tk.Label(self,anchor=tk.W,text='Año')
         lbl_anio.pack(side=tk.TOP,expand=True,fill=tk.X)
-        txt_anio = TextField(self,height=30,width=300)
-        txt_anio.pack(side=tk.TOP,expand=True,fill=tk.X, pady=5)
+        self.txt_anio = TextField(self,height=30,width=300)
+        self.txt_anio.pack(side=tk.TOP,expand=True,fill=tk.X, pady=5)
 
-        img_poster = Image(self,'',width=300,height=300)
-        img_poster.pack(side=tk.TOP,expand=True,fill=tk.BOTH,pady=10)
+        self.img_poster = Image(self,'','',width=300,height=300)
+        self.img_poster.pack(side=tk.TOP,expand=True,fill=tk.BOTH,pady=10)
 
         frm_buttons = tk.Frame(self,relief=tk.GROOVE)
         frm_buttons.pack(side=tk.TOP,expand=True,fill=tk.X)
